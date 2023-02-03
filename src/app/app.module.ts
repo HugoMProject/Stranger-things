da612662cfbc15ea8df5772f18d2f1a8388f7df6
HugoMProject <hugomoyano1996@gmail.com>
2023-02-03 15:01:37 -0300

    se termino los estilos de la pagina de acuerdo al boceto de figma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { SelectCardService } from './shared/select-card.service';
 import { SectionModule } from './sections/section/section.module';
 
 
@@ -18,7 +18,7 @@ import { SectionModule } from './sections/section/section.module';
     BrowserAnimationsModule,
     SectionModule
   ],
-  providers: [],
+  providers: [SelectCardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
