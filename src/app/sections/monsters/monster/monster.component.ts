e8ca5ba0af673e70bc5f4065d8344c27073d0e11
HugoMProject <hugomoyano1996@gmail.com>
2023-02-09 21:06:49 -0300

    se completo la logica de los componentes de la seccion de monsters

--- src/app/sections/monsters/monster/monster.component.ts
+++ src/app/sections/monsters/monster/monster.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-monster',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monster.component.css']
 })
 export class MonsterComponent implements OnInit {
+public dataMonster:Array<any>=[
+  {id:1,name:'Demogorgon',description:'Criatura humanoide depredadora'},
+  {id:2,name:'Mind Flyer',description:'Controla la mente de las personas'},
+  {id:3,name:'Vecna',description:'Posee a sus víctimas y se alimenta de sus traumas mas oscuros'}
+]
+  constructor(private _snackBar:MatSnackBar) { }
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  openSnackBar(monster:string){
+    const Monster = monster
+    const result = this.dataMonster.find(monster=>monster.id == Monster);
+    const {name,description } =result
+    this._snackBar.open(`${name} :  ${description} `,'Salir')
   }
-
 }
